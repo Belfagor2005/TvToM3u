183232ed0e1a6ad8a21927d7c8579c98b84ef598
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-10 20:32:29 +0200

    reskinned

--- usr/lib/enigma2/python/Plugins/Extensions/TvToM3u/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TvToM3u/plugin.py
@@ -14,23 +14,26 @@ from . import _, paypal
 from Components.ActionMap import ActionMap
 from Components.Label import Label
 from Components.MenuList import MenuList
-from Components.MultiContent import MultiContentEntryText
-from Components.MultiContent import MultiContentEntryPixmapAlphaTest
+from Components.MultiContent import (MultiContentEntryText, MultiContentEntryPixmapAlphaTest)
 from Plugins.Plugin import PluginDescriptor
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
-from enigma import addFont
-from enigma import RT_HALIGN_LEFT, RT_VALIGN_CENTER
-from enigma import getDesktop
-from enigma import loadPNG, gFont
-from enigma import eListboxPythonMultiContent
-from os import path, sys
+from enigma import (
+    addFont,
+    RT_HALIGN_LEFT,
+    RT_VALIGN_CENTER,
+    getDesktop,
+    loadPNG,
+    gFont,
+    eListboxPythonMultiContent,
+    )
+from os import (path, sys)
 from os.path import exists as file_exists
 import codecs
 import glob
 import os
 import re
-global new_bouquet, skin_m3up, downloadfree
+global downloadfree
 
 Version = '1.9'
 title_plug = '..:: Enigma2 M3U Converter Bouquet V. %s ::..' % Version
@@ -40,12 +43,14 @@ iconpic = plugin_path + '/plugin.png'
 tmp_bouquet = plugin_path + '/tmp'
 new_bouquet = tmp_bouquet + '/bouquets.tv'
 downloadfree = "/tmp/tvtom3u/"
-skin_m3up = os.path.join(res_plugin_path, "hd/")
+
 screenwidth = getDesktop(0).size()
 if screenwidth.width() == 1920:
     skin_m3up = res_plugin_path + 'fhd/'
 if screenwidth.width() == 2560:
     skin_m3up = res_plugin_path + 'uhd/'
+else:
+    skin_m3up = res_plugin_path + "hd/"
 if os.path.exists('/var/lib/dpkg/info'):
     skin_m3up = skin_m3up + 'dreamOs/'
 
@@ -61,7 +66,6 @@ if not os.path.exists('/tmp/tvtom3u/'):
     except OSError as e:
         print ('Error creating directory tvtom3u')
 
-downloadfree = '/tmp/'
 try:
     from Components.UsageConfig import defaultMoviePath
     downloadfree = defaultMoviePath()
@@ -73,7 +77,6 @@ except:
 class MenuListSelect(MenuList):
     def __init__(self, list):
         MenuList.__init__(self, list, True, eListboxPythonMultiContent)
-
         if screenwidth.width() == 2560:
             self.l.setFont(0, gFont('Regular', 48))
             self.l.setItemHeight(56)
@@ -216,7 +219,6 @@ class TvToM3u(Screen):
         self['version'] = Label(_('Version %s by Lululla' % Version))
         self['Key_Green'] = Label(_('Convert') + ' IPTV %s' % downloadfree)
         self['Key_Yellow'] = Label(_('Backup') + ' IPTV %s' % downloadfree)
-        # self['Key_Blue'] = Label(' ')
         self['Key_Red'] = Label(_('Exit'))
         self["paypal"] = Label()
         self['MENU'] = MenuListSelect([])
@@ -253,14 +255,9 @@ class TvToM3u(Screen):
         if screenwidth.width() == 2560:  # 1770
             res.append(MultiContentEntryPixmapAlphaTest(pos=(10, 15), size=(24, 24), png=loadPNG(icon)))
             res.append(MultiContentEntryText(pos=(60, 0), size=(1500, 54), font=0, text=name, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
-            # res.append(MultiContentEntryText(pos=(0, 0), size=(0, 0), font=0, text=dir, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
-            # res.append(MultiContentEntryText(pos=(0, 0), size=(0, 0), font=0, text=value, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
-
         elif screenwidth.width() == 1920:  # 1335
             res.append(MultiContentEntryPixmapAlphaTest(pos=(10, 12), size=(24, 24), png=loadPNG(icon)))
             res.append(MultiContentEntryText(pos=(60, 0), size=(1200, 50), font=0, text=name, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
-            # res.append(MultiContentEntryText(pos=(0, 0), size=(0, 0), font=0, text=dir, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
-            # res.append(MultiContentEntryText(pos=(0, 0), size=(0, 0), font=0, text=value, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
         else:  # 890
             res.append(MultiContentEntryPixmapAlphaTest(pos=(10, 9), size=(24, 24), png=loadPNG(icon)))
             res.append(MultiContentEntryText(pos=(60, 0), size=(800, 40), font=0, text=name, flags=RT_HALIGN_LEFT | RT_VALIGN_CENTER))
@@ -279,40 +276,36 @@ class TvToM3u(Screen):
         if url == -1 or None:
             return
         else:
-            try:
-                for dir, name, value in self.ListSelect.TvList():
-                    if url == name:
-                        WriteBouquet = ''
-                        url = tmp_bouquet + '/%s' % dir
-                        with codecs.open(url, "r", encoding="utf-8") as f:
-                            content = f.read()
-                        regexcat = '#SERVICE.*?(.*?)\\n#DESCRIPTION (.*?)\\n'
-                        match = re.compile(regexcat).findall(content)
-                        nameM3u = name.replace(' ', '').lower()
-                        FILE_M3U = '%s/%s.m3u' % (downloadfree, nameM3u)
-                        if sys.version_info[0] == 3:
-                            WriteBouquet = open(FILE_M3U, 'w', encoding='UTF-8')
-                        else:
-                            WriteBouquet = open(FILE_M3U, 'w')
-                        WriteBouquet.write('#EXTM3U\n')
-                        for url, name in match:
-                            n1 = url.find("http", 0)
-                            if n1 > - 1:
-                                url = url[n1:]
-                                url = url.replace('%3a', ':')
-                                WriteBouquet.write('#EXTINF:-1 tvg-ID="" tvg-name="%s" tvg-logo="" group-title="",%s\n' % (name, name))
-                            n3 = url.find("rtmp", 0)
-                            if n3 > - 1:
-                                url = url[n3:]
-                                url = url.replace('%3a', ':')
-                                WriteBouquet.write('#EXTINF:-1 tvg-ID="" tvg-name="%s" tvg-logo="" group-title="",%s\n' % (name, name))
-                            WriteBouquet.write('%s\n' % url)
-                        f.close()
-                        WriteBouquet.close()
-                self.session.open(MessageBox, _('Export Succes'), MessageBox.TYPE_INFO, timeout=8)
-            except Exception as e:
-                print('++++++++++ERROR CONVERT+++++++++++++')
-                print('++++++++++%s ', e)
+            for dir, name, value in self.ListSelect.TvList():
+                if url == name:
+                    WriteBouquet = ''
+                    url = tmp_bouquet + '/%s' % dir
+                    with codecs.open(url, "r", encoding="utf-8") as f:
+                        content = f.read()
+                    regexcat = '#SERVICE.*?(.*?)\\n#DESCRIPTION (.*?)\\n'
+                    match = re.compile(regexcat).findall(content)
+                    nameM3u = name.replace(' ', '').lower()
+                    FILE_M3U = '%s/%s.m3u' % (downloadfree, nameM3u)
+                    if sys.version_info[0] == 3:
+                        WriteBouquet = open(FILE_M3U, 'w', encoding='UTF-8')
+                    else:
+                        WriteBouquet = open(FILE_M3U, 'w')
+                    WriteBouquet.write('#EXTM3U\n')
+                    for url, name in match:
+                        n1 = url.find("http", 0)
+                        if n1 > - 1:
+                            url = url[n1:]
+                            url = url.replace('%3a', ':')
+                            WriteBouquet.write('#EXTINF:-1 tvg-ID="" tvg-name="%s" tvg-logo="" group-title="",%s\n' % (name, name))
+                        n3 = url.find("rtmp", 0)
+                        if n3 > - 1:
+                            url = url[n3:]
+                            url = url.replace('%3a', ':')
+                            WriteBouquet.write('#EXTINF:-1 tvg-ID="" tvg-name="%s" tvg-logo="" group-title="",%s\n' % (name, name))
+                        WriteBouquet.write('%s\n' % url)
+                    f.close()
+                    WriteBouquet.close()
+            self.session.open(MessageBox, _('Export Succes'), MessageBox.TYPE_INFO, timeout=8)
 
     def keyYellow(self):
         iptv_to_save = lista_bouquet()
